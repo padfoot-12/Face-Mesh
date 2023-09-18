6bcc7020107456b85505cd2744b33a10cffd2f16
padfoot-12 <pratikRaj1212.01@gmail.com>
2023-09-18 21:34:31 +0530

    Detect function is set up

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,35 @@ function App() {
   const runFacemesh = async () => {
     const net = await facemesh.load({
       inputResolution:{width:640, height:480}, scale:0.8
-    })
+    });
+    setInterval(()=>{
+      detect(net)
+    }, 100)
+  };
+
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current!== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState ===4
+    ) {
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      const face = await net.estimateFaces(video);
+      console.log(face);
+    }
   }
-  
+
+  runFacemesh();
+
   return (
     <div className="App">
       <header className="App-header">
